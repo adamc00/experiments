1f2a5d9ac4014ed9d6b171cdee4fa7d377a66d4e
adamc00 <adamc@strategicdata.com.au>
2022-08-12 11:32:59 +1000

    Conditional psql

--- sqitch/variables/deploy/database.sql
+++ sqitch/variables/deploy/database.sql
@@ -1,9 +1,14 @@
 -- Deploy sqitch-variables:database to pg
+
+\set ON_ERROR_STOP 1
 \set db_name db_:deployment
 
--- https://stackoverflow.com/questions/18389124/simulate-create-database-if-not-exists-for-postgresql
-select format('create database %I', :'db_name')
-where not exists (
-    select from pg_database where datname = :'db_name'
-)\gexec
+-- Create a boolean for use in \if
+-- https://www.postgresql.org/message-id/CAOBaU_ZeHj6FWJQiOMOKoxoe7DXLshoaCoLL0w1b3hpg%2B%2BVJRQ%40mail.gmail.com
+SELECT :'deployment' = 'development' AS is_development \gset
+\if :is_development
+    create database :db_name;
+\endif
+
+
 
